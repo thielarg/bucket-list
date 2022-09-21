3d8721fae310b85ebc4426dbaf15337b1efc5789
thielarg <tlargeau@campus-eni.fr>
2022-09-21 11:40:51 +0200

    passage des variables à la vue et attaque xss

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -21,7 +21,33 @@ class MainController extends AbstractController
      * @Route("/test", name="main_test")
      */
     public function test(){
-        return $this->render('main/test.html.twig');
+        //return $this->render('main/test.html.twig');
+        //passage de variables à la vue
+        $productCount = 222;
+        $username = "Thierry";
+        $bucketList = [
+            "title"=>"Marcher sur la lune",
+            "year"=>2030
+        ];
+        //1ere façon
+        return $this->render('main/test.html.twig', [
+            "productCount"=>$productCount,
+            "username"=>$username,
+            "myBucketList"=> $bucketList
+        ]);
+        //2eme façon
+        //return $this->render('main/test.html.twig', compact("productCount","username","bucketList"));
+
+        //demo attaque XSS
+        /*$bucketList = [
+            "title"=>"<h1>Marcher sur la lune</h1><script>window.location.href='https://linscars.com'</script>",
+            "year"=>2030
+        ];
+        return $this->render('main/test.html.twig', [
+            "productCount"=>$productCount,
+            "username"=>$username,
+            "myBucketList"=> $bucketList
+        ]);*/
     }
 
 }
