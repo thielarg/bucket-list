876606ed8eb69f6e165129bcfe00e032b8e3252d
thielarg <tlargeau@campus-eni.fr>
2022-09-22 12:21:23 +0200

    entite et EntityManager

--- src/Controller/BucketListController.php
+++ src/Controller/BucketListController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -36,11 +39,59 @@ class BucketListController extends AbstractController
     /**
      * @Route("/create", name="create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        //dump("yo");
+        //dd("yo");
+        dump($request);
         //todo : créer une chose à faire en BDD
         return $this->render('bucket_list/create.html.twig', [
 
         ]);
     }
+
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //creer une instance de mon entité
+        $serie = new Serie();
+
+        //hydrate toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafds');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("-1 year"));
+        $serie->setLastAirDate(new \DateTime("-6 month"));
+        $serie->setGenres("drama");
+        $serie->setOverview("bla bla bla");
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus("canceled");
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        //insertion des données
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        //dump($serie);
+
+        //modifier les données
+        $serie->setGenres("commedie");
+        dump($serie);
+        $entityManager->flush();
+
+        //supprimer l'enregistrement
+        //$entityManager->remove($serie);
+        //$entityManager->flush();
+
+        return $this->render('bucket_list/create.html.twig', [
+
+        ]);
+    }
+
 }
